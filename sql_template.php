c8cd9b4138c35104143ec28d9fb648bc7bca9543
ryuki0914 <ryuki20040915@icloud.com>
2025-06-03 09:35:23 +0900

    sqlテンプレート挿入

---
+++ sql_template.php
@@ -0,0 +1,22 @@
+<?php
+    //sql接続するための情報を変数に挿入
+    $host = 'localhost';
+    $dbname = 'bbs';          
+    $user = 'root';          
+    $pass = 'root';   
+
+//try_catch文の中にSQLに接続するコードを書いてる、覚える必要はないためコピペで良い
+try {
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+ 
+        $pdo = new PDO($dsn, $user, $pass);
+ 
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+        echo "接続成功<br>";
+    } catch (PDOException $e) {
+        echo "接続失敗: " . $e->getMessage();
+    }
+
+    
+?>
